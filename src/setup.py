2c05c48abd009bf6abc4b01f44d77c0a1059fd93
Weijia Sun <weijia_sun@163.com>
2018-03-20 08:10:44 +0800

    rename aspicker

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name="aspicker",
+      version=1.0,
+      description="Adaptive Stacking Picker",
+      author="",
+      author_email="weijia_sun@163.com",
+      url="https://github.com/sun031/aspicking",
+      py_modules=["aspicker"],
+      )
